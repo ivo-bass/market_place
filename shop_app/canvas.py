a6f8aaac5eeaaf8019d0f18561cb8fb9a475aafb
ivo-bass <ivailo.ignatoff@gmail.com>
2021-02-03 18:48:25 +0200

    add canvas and main files

---
+++ shop_app/canvas.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+
+def create_app():
+    tk = Tk()
+    tk.geometry('700x500')
+    tk.title('Shop')
+    return tk
+
+
+app = create_app()
---
+++ shop_app/main.py
@@ -0,0 +1,5 @@
+from canvas import app
+
+
+if __name__ == '__main__':
+    app.mainloop()
