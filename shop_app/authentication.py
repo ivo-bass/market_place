4af6e79f613b09918cdcce1b84b08ecb846996ae
ivo-bass <ivailo.ignatoff@gmail.com>
2021-02-04 05:06:55 +0200

    freeze to requirements, optimize imports and paths

--- shop_app/authentication.py
+++ shop_app/authentication.py
@@ -1,11 +1,17 @@
 import json
-from tkinter import Button, Entry, Label
+import os
 from shop_app.canvas import app
 from shop_app.helpers import clean_screen
 from shop_app.products import render_products
+from tkinter import Button, Entry, Label
 
-USERS_FILE = 'db/users.txt'
-CREDENTIALS_FILE = 'db/user_credentials_db.txt'
+BASE_DIR = os.path.dirname(__file__)
+IMAGE_DIR = os.path.join(BASE_DIR, 'images')
+DB_DIR = os.path.join(BASE_DIR, 'db')
+PRODUCTS_FILE = os.path.join(DB_DIR, 'products.txt')
+USERS_FILE = os.path.join(DB_DIR, 'users.txt')
+CURRENT_USER_FILE = os.path.join(DB_DIR, 'current_user.txt')
+CREDENTIALS_FILE = os.path.join(DB_DIR, 'user_credentials_db.txt')
 ERROR_CREDENTIALS = 'Please enter valid credentials!'
 
 
@@ -15,7 +21,9 @@ def login(username, password):
         for line in lines:
             u, p = line[:-1].split(' | ')
             if u == username and p == password:
-                render_products()
+                with open(CURRENT_USER_FILE, 'w') as f:
+                    f.write(username)
+                    render_products()
                 return
         render_login(err=ERROR_CREDENTIALS)
 
@@ -33,38 +41,60 @@ def register(**user):
 
 def render_login(err=None):
     clean_screen()
+
+    label = Label(text='Username: ')
+    label.grid(row=0, column=0)
     username = Entry(app)
-    username.grid(row=0, column=0)
+    username.grid(row=0, column=1)
+
+    label = Label(text='Password: ')
+    label.grid(row=1, column=0)
     password = Entry(app, show='*')
-    password.grid(row=1, column=0)
+    password.grid(row=1, column=1)
     Button(app, text='Submit', bg='green', fg='white',
            command=lambda: login(
                username=username.get(),
-               password=password.get())).grid(row=3, column=0)
+               password=password.get())).grid(row=3, column=1)
     if err:
-        Label(text=err).grid(row=4, column=0)
+        Label(text=err).grid(row=4, column=1)
 
 
 def render_register():
     # Todo: retype password
-    # Todo: Label for fields left of field
     clean_screen()
+
+    label = Label(text='Username: ')
+    label.grid(row=0, column=0)
     username = Entry(app)
-    username.grid(row=0, column=0)
+    username.grid(row=0, column=1)
+
+    label = Label(text='Password: ')
+    label.grid(row=1, column=0)
     password = Entry(app, show='*')
-    password.grid(row=1, column=0)
+    password.grid(row=1, column=1)
+
+    label = Label(text='First Name: ')
+    label.grid(row=2, column=0)
     first_name = Entry(app)
-    first_name.grid(row=2, column=0)
+    first_name.grid(row=2, column=1)
+
+    label = Label(text='Last Name: ')
+    label.grid(row=3, column=0)
     last_name = Entry(app)
-    last_name.grid(row=3, column=0)
+    last_name.grid(row=3, column=1)
+
     Button(app, text='Submit', bg='green', fg='white',
            command=lambda: register(
                username=username.get(),
                password=password.get(),
                first_name=first_name.get(),
-               last_name=last_name.get())).grid(row=4, column=0)
+               last_name=last_name.get())).grid(row=4, column=1)
 
 
 def render_main_enter_screen():
-    Button(app, text='LogIn', bg='green', fg='white', command=render_login).grid(row=0, column=0)
-    Button(app, text='Register', bg='yellow', fg='black', command=render_register).grid(row=0, column=1)
+    label = Label(text='Welcome to the shop!')
+    label.grid(row=1, column=2)
+    l_btn = Button(app, text='LogIn', bg='green', fg='white', command=render_login)
+    l_btn.grid(row=2, column=1)
+    r_btn = Button(app, text='Register', bg='yellow', fg='black', command=render_register)
+    r_btn.grid(row=2, column=2)
--- shop_app/products.py
+++ shop_app/products.py
@@ -1,10 +1,50 @@
 import json
-
+import os
+from PIL import Image, ImageTk
 from shop_app.canvas import app
 from shop_app.helpers import clean_screen
 from tkinter import Button, Label
 
-PRODUCTS_FILE = 'db/products.txt'
+BASE_DIR = os.path.dirname(__file__)
+DB_DIR = os.path.join(BASE_DIR, 'db')
+IMAGE_DIR = os.path.join(BASE_DIR, 'images')
+PRODUCTS_FILE = os.path.join(DB_DIR, 'products.txt')
+USERS_FILE = os.path.join(DB_DIR, 'users.txt')
+CURRENT_USER_FILE = os.path.join(DB_DIR, 'current_user.txt')
+
+
+def update_products(pr_id):
+    with open(PRODUCTS_FILE, 'r+') as file:
+        products = file.readlines()
+        file.seek(0)
+        for pr in products:
+            product = json.loads(pr)
+            if product.get('id') == pr_id:
+                product['count'] -= 1
+            # TODO: if product count == 0 logic
+            file.write(json.dumps(product))
+            file.write('\n')
+
+
+def update_users(logged_user, pr_id):
+    with open(USERS_FILE, 'r+') as file:
+        users = file.readlines()
+        file.seek(0)
+        for us in users:
+            user = json.loads(us)
+            if user.get('username') == logged_user:
+                user['products'].append(pr_id)
+            file.write(json.dumps(user))
+            file.write('\n')
+
+
+def buy_product(btn):
+    pr_id = int(btn.cget('text').split()[-1])
+    with open(CURRENT_USER_FILE, 'r') as file:
+        logged_user = file.read()
+    update_users(logged_user, pr_id)
+    update_products(pr_id)
+    render_products()
 
 
 def render_products():
@@ -14,7 +54,22 @@ def render_products():
         col = 0
         for pr in products:
             product = json.loads(pr)
-            Label(text=product.get('name')).grid(row=0, column=col)
 
-            Button(app, text='Buy').grid(row=2, column=col)
+            label = Label(text=product.get('name'))
+            label.grid(row=0, column=col)
+
+            image = Image.open(os.path.join(IMAGE_DIR, product.get('img_path')))
+            image = image.resize((200, 200))
+            photo = ImageTk.PhotoImage(image)
+            img_label = Label(image=photo)
+            img_label.image = photo
+            img_label.grid(row=1, column=col)
+
+            label = Label(text=f'In Stock: {product.get("count")}')
+            label.grid(row=3, column=col)
+
+            button = Button(app, text=f'Buy {product.get("id")}')
+            button.configure(command=lambda b=button: buy_product(b))
+            button.grid(row=4, column=col)
+
             col += 1
